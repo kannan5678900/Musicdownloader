1b8472adc8d6b40a8b7c8591d30c10cd02ea5fc0
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-08 14:26:28 +0530

    Update video.py

--- plugins/video.py
+++ plugins/video.py
@@ -17,7 +17,7 @@ def progress(current, total):
     print(f"{current * 100 / total:.1f}%")
 
 @Client.on_message(filters.command(["video"]))
-def video(client, message):
+def a(client, message):
     query = ''
     for i in message.command[1:]:
         query += ' ' + str(i)
