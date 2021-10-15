ba9245eb634c66278149cdd839175e2bcea557f9
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-15 20:28:56 +0530

    Create text-to-speech.py

---
+++ plugins/text-to-speech.py
@@ -0,0 +1,41 @@
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
+
+from googletrans import Translator
+from gtts import gTTS
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+
+def convert(text):
+    audio = BytesIO()
+    i = Translator().translate(text, dest="en")
+    lang = i.src
+    tts = gTTS(text, lang=lang)
+    audio.name = lang + ".mp3"
+    tts.write_to_fp(audio)
+    return audio
+
+
+@Client.on_message(filters.command("tts"))
+async def text_to_speech(client, message: Message):
+    if not message.reply_to_message:
+        return await message.reply_text("`Please Reply to Some Text...`✌️")
+    if not message.reply_to_message.text:
+        return await message.reply_text("`This is Not Text Message...`😡")
+    chat_id = message.chat.id
+    m = await message.reply_text("`Processing...`")
+    text = message.reply_to_message.text
+    caption = f"**Identified by** : @Musicdownloadv2bot"
+    try:
+        loop = get_running_loop()
+        audio = await loop.run_in_executor(None, convert, text)
+        await client.send_chat_action(chat_id, "upload_audio")
+        await message.reply_audio(audio, performer='Musicdownloadv2bot', caption=caption)
+        await m.delete()
+        audio.close()
+    except Exception as e:
+        await m.edit(str(e))
+        es = traceback.format_exc()
+        print(es)
