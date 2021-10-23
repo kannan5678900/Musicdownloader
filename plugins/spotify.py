b38c3836b6cede7aa529b808bd92ea6850e83e72
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-23 11:07:05 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -23,7 +23,7 @@ def send_spotify_songs(client, message: Message):
         print(e)
         msg.edit(f'**Found Nothing to your Query :** `{song_link}` ❌')
         return
-    msg.edit(f"`Uploading Your Song From [Spotify](http://Spotify.com)...`\n__Please Wait for Some Seconds__[😪](https://telegra.ph/file/1773306efae08a9edfa89.jpg)")
+    msg.edit(f"`Uploading Your Song From **Spotify**...`\n__Please Wait for Some Seconds__[😪](https://telegra.ph/file/1773306efae08a9edfa89.jpg)")
     spotdl.download_from_spotify(download_path, song_link)
     spotdl.send_songs_from_directory(download_path, client, message)
     msg.delete()
