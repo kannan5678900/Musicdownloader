a95d3859ef3e0ea1eeab786b5df3e3870fcab88f
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-09 14:00:54 +0530

    Update yt_search.py

--- plugins/yt_search.py
+++ plugins/yt_search.py
@@ -27,7 +27,7 @@ async def yt_search(client, message):
     try:
         full_response = await ytsearch(query, limit=lim)
     except Exception as e:
-        return await message.edit(video, str(e), time=10, parse_mode=_format.parse_pre)
+        return await message.edit(video, str(e), time=10, parse_mode=format.parse_pre)
     text = f"**•  Search Query:**\n`{query}`\n\n**•  Results:**\n{full_response}"
     await message.reply_text(text=text)
     await video.delete()
