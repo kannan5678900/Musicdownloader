b600e91c6c0305f18133c2d8e47a1988bb085802
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-15 21:04:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 from pyrogram import Client
 import os
+from os import path
+from shazamio import Shazam, exceptions, FactoryArtist, FactoryTrack
+
+shazam = Shazam()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "6"))
@@ -17,3 +21,34 @@ if __name__ == "__main__" :
         plugins=plugins
     )
     bot.run()
+
+class bot(Client):
+    
+    async def recognize(self, path):
+        return await shazam.recognize_song(path)
+
+    async def related(self, track_id):
+        try:
+            return (await shazam.related_tracks(track_id=track_id, limit=50, start_from=2))['tracks']
+        except exceptions.FailedDecodeJson:
+            return None
+    
+    async def get_artist(self, query: str):
+        artists = await shazam.search_artist(query=query, limit=50)
+        hits = []
+        try:
+            for artist in artists['artists']['hits']:
+                hits.append(FactoryArtist(artist).serializer())
+            return hits
+        except KeyError:
+            return None
+        
+    async def get_artist_tracks(self, artist_id: int):
+        tracks = []
+        tem = (await shazam.artist_top_tracks(artist_id=artist_id, limit=50))['tracks']
+        try:
+            for track in tem:
+                tracks.append(FactoryTrack(data=track).serializer())
+            return tracks
+        except KeyError:
+            return None
