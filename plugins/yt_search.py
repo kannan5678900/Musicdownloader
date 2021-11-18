90cd3e645fd772e681f5af802e95e004f0ebb556
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-18 07:37:07 +0530

    Update yt_search.py

--- plugins/yt_search.py
+++ plugins/yt_search.py
@@ -6,7 +6,7 @@ from plugins.google import get_text
 
 async def ytsearch(query, limit):
     result = ""
-    videolinks = SearchVideos(query.lower(), limit=10, offset=1, mode="dict")
+    videolinks = SearchVideos(query.lower(), max_results=limit, offset=1, mode="dict")
     for v in videolinks.result()["result"]:
         textresult = f"[{v['title']}](https://www.youtube.com/watch?v={v['id']})\n"
         try:
@@ -25,7 +25,7 @@ async def yt_search(client, message):
     video = await message.reply_text("`Searching...`")
     lim = 10
     try:
-        full_response = await ytsearch(query, limit=lim)
+        full_response = await ytsearch(query, max_results=lim)
         text = f"**•  Search Query:**\n`{query}`\n\n**•  Results:**\n{full_response}"
         await message.reply_text(text=text)
         await video.delete()
