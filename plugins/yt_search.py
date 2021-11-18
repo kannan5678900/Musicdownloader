201543585fd8c18bb549a01473d51a25983e4b99
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-18 07:50:33 +0530

    Update yt_search.py

--- plugins/yt_search.py
+++ plugins/yt_search.py
@@ -1,21 +1,8 @@
 from pyrogram import Client, filters
 
-from youtubesearchpython import SearchVideos
+from youtube_search import YoutubeSearch
 
 from plugins.google import get_text
-
-async def ytsearch(query, limit):
-    result = ""
-    videolinks = SearchVideos(query.lower(), max_results=limit, offset=1, mode="dict")
-    for v in videolinks.result()["result"]:
-        textresult = f"[{v['title']}](https://www.youtube.com/watch?v={v['id']})\n"
-        try:
-            textresult += f"**Description : **`{v['descriptionSnippet'][-1]['text']}`\n"
-        except Exception:
-            textresult += "**Description : **`None`\n"
-        textresult += f"**Duration : **__{v['duration']}__  **Views : **__{v['viewCount']['short']}__\n"
-        result += f"☞ {textresult}\n"
-    return result
 
 @Client.on_message(filters.command(["yts", "ytsearch"]))
 async def yt_search(client, message):
@@ -28,10 +15,10 @@ async def yt_search(client, message):
         i = 0
         text = ""
         while i < 10:
-            text += f"🏷 **Name:** __{results[i]['title']}__\n"
+            text += f"🍁 **Name:** __{results[i]['title']}__\n"
             text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
             text += f"👀 **Views:** `{results[i]['views']}`\n"
-            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"💥 **Channel:** {results[i]['channel']}\n"
             text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await message.reply_chat_action("typing")
