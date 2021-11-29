3df15fbdb352976e2e7ad4c1abce971dd5dc24b3
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-11-29 10:17:20 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -15,18 +15,20 @@ def send_spotify_songs(client, message: Message):
     msg = message.reply_text("`Processing...`")
     song_link = get_text(message)
     chat_id = message.chat.id
+    download_path = os.getcwd() + "/" + str(uuid.uuid4())
     if not song_link:
         return msg.edit("**Invalid Format ⛔\nEg** : `/spotify Believer`\n\n`/spotify https://open.spotify.com/track/0pqnGHJpmpxLKifKRmU6WP`")
-    try:
-        download_path = os.getcwd() + "/" + str(uuid.uuid4())
-    except Exception as e:
+    try:       
+        msg.edit(f"`Uploading Your Song From` **Spotify...\nPlease Wait for Some Seconds**[😪](https://telegra.ph/file/2c42b889a0dfb0c24f27a.jpg)")
+        spotdl.download_from_spotify(download_path, song_link)
+        spotdl.send_songs_from_directory(download_path, client, message)
+        try:
+            msg.delete()
+            print(song_link)
+        except Exception as e:
+        msg.edit('😔 𝙵𝚊𝚒𝚕𝚎𝚍\n\n𝚁𝚎𝚙𝚘𝚛𝚝 𝚃𝚑𝚒𝚜 𝙴𝚛𝚛𝚘𝚛 𝚝𝚘 𝙵𝚒𝚡 @Peterparker6 🧡')
         print(e)
-        msg.edit(f'**Found Nothing to your Query :** `{song_link}` ❌')
-        return
-    msg.edit(f"`Uploading Your Song From` **Spotify...\nPlease Wait for Some Seconds**[😪](https://telegra.ph/file/1773306efae08a9edfa89.png)")
-    spotdl.download_from_spotify(download_path, song_link)
-    spotdl.send_songs_from_directory(download_path, client, message)
-    msg.delete()
-    print(song_link)
-    es = traceback.format_exc()
-    print(es)
+    except Exception as e:
+        msg.edit(f'Failed to download your **Query** - {song_link}')
+        print(e)
+  
