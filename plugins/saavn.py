c385191d31df9d70e7adde70ba9972e969feb0c9
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-12-03 07:21:56 +0530

    Realme

--- plugins/saavn.py
+++ plugins/saavn.py
@@ -39,6 +39,7 @@ async def saavn(client, message):
             await client.send_chat_action(chat_id, "upload_audio")
             await message.reply_audio(audio=ffile, title=song, performer=singer, caption=iron_man, reply_markup=buttons, quote=True)
             await msg.delete()
+            print(query)
     except Exception as e:
         await msg.edit("⚠️ **Currently Not Support Links.\nPlease try again with any other Query**")    
         print(e)
