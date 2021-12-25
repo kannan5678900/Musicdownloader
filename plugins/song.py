201e86a8f9ad0ca7d02bb826d2b56b51daa796aa
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-12-25 22:15:15 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -69,7 +69,7 @@ async def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        ironman = f'🍁 **Tittle** : __{title}__\n🍁 **Channel** : `{thor}`\n🍁 **Requested For** : `{query}`'
+        ironman = f'❄️ **Tittle** : __{title}__\n❄️ **Channel** : `{thor}`\n❄️ **Requested For** : `{query}`'
         rep = f"🎧 𝗧𝗶𝘁𝘁𝗹𝗲 : [{title[:35]}]({link})\n⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻 : `{duration}`\n👀 𝗩𝗶𝗲𝘄𝘀 : `{views}`\n\n📮 **By** : [{message.from_user.first_name}](tg://user?id={message.from_user.id})\n📤 𝗕𝘆 : [Music Downloader 🎶](https://t.me/MusicDownloadv2bot)"
         buttons = InlineKeyboardMarkup([[InlineKeyboardButton('sᴇᴀʀᴄʜ ɪɴʟɪɴᴇ', switch_inline_query_current_chat=f'yt ')]])
         secmul, dur, dur_arr = 1, 0, duration.split(':')
