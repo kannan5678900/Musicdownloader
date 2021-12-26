174a21cc8988a4a2ac1805290e10a5c844de9b63
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-12-26 17:51:08 +0530

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -1,5 +1,6 @@
 import os
 import yt_dlp
+import ffmpeg
 from pyrogram import Client, filters
 
 from plugins.google import get_text
@@ -11,11 +12,26 @@ async def down(client, message):
     if not query:
         await msg.edit("**എനിക്ക് എന്തെങ്കിലും താടാ**")
         return
-    video = {}
+    video = {
+         "addmetadata": False,
+         "key": "FFmpegMetadata",
+         "prefer_ffmpeg": True,
+         "geo_bypass": True,
+         "nocheckcertificate": True,
+         "postprocessors": [
+             {
+                "key": "FFmpegVideoConvertor",
+                "preferredcodec": "mp4",
+                "preferredquality": "720",
+             },
+          ],
+          "outtmpl": "%(title)s.mp4",
+          "quiet": True,
+    }
     try:
         with yt_dlp.YoutubeDL(video) as ydl:
             he = ydl.download(query)
-        await client.send_file(he)
+        await message.reply_video(video=he)
         await msg.delete()
     except Exception as e:
         await msg.edit(f'😔**Failed**\n\n__Report this Error to my [Master](https://t.me/Peterparker6)\nOr try__ : `/spotify {query}`')
