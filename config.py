689d22d63c1f02778dc5c5edca51a0201e19ce0b
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-01-31 08:08:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from os import getenv
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
@@ -8,3 +9,5 @@ AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "NACBots")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+ARQ_API_KEY = getenv("ARQ_API_KEY")
+ARQ_API_URL = "https://grambuilders.tech/"
