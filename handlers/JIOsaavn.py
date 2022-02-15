37e8a52c7affe958f93959bd050a8add8be650f6
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-02-15 13:15:03 +0530

    Update JIOsaavn.py

--- handlers/JIOsaavn.py
+++ handlers/JIOsaavn.py
@@ -1,9 +1,9 @@
 import aiohttp
 import requests
 
-JIO = f"http://starkmusic.herokuapp.com/result/?query={query}"
+JIO = "http://starkmusic.herokuapp.com/result/?query={}"
 
 async def JIO(query: str):
-    async with requests.get(url=JIO) as res:
+    async with requests.get(url=JIO(query)) as res:
         return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
 
