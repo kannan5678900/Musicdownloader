8da9ebfb8f090a9c2fd5fde520fad4ce3e0d1237
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-03-03 05:27:46 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -21,6 +21,7 @@ def send_spotify_songs(client, message: Message):
     if not song_link:
         return msg.edit("**Invalid Format ⛔\nEg** : `/spotify Believer`\n\n`/spotify https://open.spotify.com/track/0pqnGHJpmpxLKifKRmU6WP`")
     if 'https://www.shazam.com/' in song_link:
+        return msg.edit('__Hmm Strange 😑__')
     try:       
         msg.edit(f"`Uploading Your Song From` **Spotify...\nPlease Wait for Some Seconds**[😪](https://telegra.ph/file/99dfbd8791044f70db76b.jpg)")
         spotdl.download_from_spotify(download_path, song_link)
