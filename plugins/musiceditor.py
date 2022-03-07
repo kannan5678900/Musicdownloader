8568139e61a281e2f98fe3b133561b9de86b8139
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-03-07 07:37:08 +0530

    Create musiceditor.py

---
+++ plugins/musiceditor.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+import os
+import io
+from pyromod import listen
+from PIL import Image
+from music_tag import load_file
+
+@Client.on_message(filters.command(["edit"]))
+async def tag(bot, message):
+    msg = await message.reply("Downloading...")
+    audio = message.reply_to_message.audio
+    if not audio:
+        return await msg.edit("reply to some audio")
+    await message.download(f"temp/{message.audio.file_name}.mp3")
+    music = load_file(f"temp/{message.audio.file_name}.mp3")
+
+    try:
+        artwork = music['artwork']
+        image_data = artwork.value.data
+        img = Image.open(io.BytesIO(image_data))
+        img.save("temp/artwork.jpg")
+    except ValueError:
+        image_data = None
+
+    await msg.delete()
+    fname = await bot.ask(message.chat.id,'`Send the Filename`', filters=filters.text, parse_mode='Markdown')
+    title = await bot.ask(message.chat.id,'`Send the Title name`', filters=filters.text, parse_mode='Markdown')
+    artist = await bot.ask(message.chat.id,'`Send the Artist(s) name`', filters=filters.text, parse_mode='Markdown')
+    answer = await bot.ask(message.chat.id,'`Send the Artwork or` /skip', filters=filters.photo | filters.text, parse_mode='Markdown')
+    music.remove_tag('artist')
+    music.remove_tag('title')
+    music['artist'] = artist.text
+    music['title'] = title.text
+
+    if answer.photo:
+        await bot.download_media(message=answer.photo, file_name="temp/artwork.jpg")
+        music.remove_tag('artwork')
+        with open('temp/artwork.jpg', 'rb') as img_in:
+            music['artwork'] = img_in.read()
+    music.save()
+
+    try:
+        await bot.send_audio(chat_id=message.chat.id, file_name=fname.text, performer=artist.text, title=title.text, duration=m.audio.duration, audio=f"temp/{m.audio.file_name}.mp3", thumb='temp/artwork.jpg' if answer.photo or image_data else None)
+    except Exception as e:
+        print(e)
+        return
+    os.remove(f"temp/{message.audio.file_name}.mp3") 
