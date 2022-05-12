02f7289ee5d5623ebcf575f9ceec36ca21210103
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-05-12 08:18:09 +0530

    Update extract_user.py

--- handlers/extract_user.py
+++ handlers/extract_user.py
@@ -31,3 +31,18 @@ def extract_user(message: Message) -> Tuple[int, str]:
         user_id, user_first_name = _eufm(message)
 
     return (user_id, user_first_name)
+
+
+def _eufm(message: Message) -> Tuple[int, str]:
+    user_id = None
+    user_first_name = None
+
+    if message.from_user:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+
+    elif message.sender_chat:
+        user_id = message.sender_chat.id
+        user_first_name = message.sender_chat.title
+
+    return (user_id, user_first_name)
