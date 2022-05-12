b2eef6e894881a00376f5a7a380b1c3505268bf4
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2022-05-12 08:17:33 +0530

    Create extract_user.py

---
+++ handlers/extract_user.py
@@ -0,0 +1,33 @@
+from pyrogram.types import Message
+from typing import Tuple
+
+
+def extract_user(message: Message) -> Tuple[int, str]:
+    """extracts the user from a message"""
+    user_id = None
+    user_first_name = None
+
+    if len(message.command) > 1:
+        if len(message.entities) > 1 and message.entities[1].type == "text_mention":
+            # 0: is the command used
+            # 1: should be the user specified
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            print("പൊട്ടൻ ")
+
+    elif message.reply_to_message:
+        user_id, user_first_name = _eufm(message.reply_to_message)
+
+    elif message:
+        user_id, user_first_name = _eufm(message)
+
+    return (user_id, user_first_name)
